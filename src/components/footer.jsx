6199b74139b46d481944cc4ac6961c9e263faa05
Eric Lee <ayrocklee@gmail.com>
2023-06-13 20:34:28 -0700

    put footer content in a `container`

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="flex items-center w-full bg-[rgb(0, 2, 18)] border-t border-white/[0.1]">
-      Ayrock - Designed By Eric Lee
+    <footer className="flex items-center w-full bg-[rgb(0, 2, 18)] border-t border-white/[0.1] h-[128px]">
+      <div className="flex container mx-auto justify-center md:justify-normal">
+        <a href="#">Ayrock - Designed By Eric Lee</a>
+      </div>
     </footer>
   );
 }
