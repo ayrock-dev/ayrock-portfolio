39c0b72ab7a574c1cd88c50d7f4015ca60c1aa4e
Eric Lee <ayrocklee@gmail.com>
2023-06-14 10:00:09 -0700

    lint

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="flex items-center w-full bg-[rgb(0, 2, 18)] border-t border-white/[0.1] h-[128px]">
-      <div className="flex container mx-auto justify-center md:justify-normal">
+    <footer className="flex h-[128px] w-full items-center border-t border-white/[0.1] bg-[rgb(0,2,18)]">
+      <div className="container mx-auto flex justify-center md:justify-normal">
         <a href="#">Ayrock - Designed By Eric Lee</a>
       </div>
     </footer>
